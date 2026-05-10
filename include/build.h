9fe832a8a767e8145d0944aca4116baa88a3073e
Alex Larbe <alexlarbe5@gmail.com>
2026-05-10 20:29:35 +0100

    oops

--- include/build.h
+++ include/build.h
@@ -268,7 +268,7 @@ Then you can use another oneliner to query all variables:
 	#define XASH_WASM 1
 #elif defined __sparc__
     #define XASH_SPARC 1
-    #define __arch64__
+    #if defined __arch64__
 		#define XASH_64BIT 1
 #else
 	#error "Place your architecture name here! If this is a mistake, try to fix conditions above and report a bug"
